0161f202029fd3f822c1b9cac4a5317151b98da1
McMaldo <maldonado.ignacio.pablo@gmail.com>
2025-03-19 15:50:52 -0300

    FEAT #016 * Hobbies design * Hobbies responsive fix * Hobbies ImgCarousel * Hobbies imgs * Hobbies types translation

--- src/component/hobbies/Hobbies.jsx
+++ src/component/hobbies/Hobbies.jsx
@@ -1,46 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import s from "./hobbies.module.css";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { hobbies } from "../../assets/userData.json";
 
+const ImgCarousel = ({article, en}) => {
+
+    const {imgList, type} = article;
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % imgList.length);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [imgList.length]);
+
+    return (
+        <article className={s.imgContainer+" "+(imgList.length > 1 ? s.imgCarousel : "")}>
+            <img src={hobbies.ARTICLES_IMGS_DIR+imgList[currentIndex]} alt="img"/>
+            <div className={s.type}>
+                <span>{en? type.en : type.es}</span>
+            </div>
+        </article>
+    );
+};
+
 export default function Hobbies() {
 
-    let [isTranslatedToEnglish, setTranslatedToEnglish] = useLocalStorage("translatedToEnglish", true);
-    let [articlesVisible, setArticlesVisible] = useState(false);
+    let [isTranslatedToEnglish] = useLocalStorage("translatedToEnglish", true);
+    let [isArticlesVisible, setArticlesVisible] = useState(false);
 
     return(
-        <div className={s.hobbies}>
-            {articlesVisible ? (
+        <div className={s.hobbies+" "+(isArticlesVisible? s.scrollable : "")}>
+            {isArticlesVisible ? (
             <>
                 <h3>{isTranslatedToEnglish? "My Hobbies" : "Mis Pasatiempos"}</h3>
                 <div className={s.hobbiesSlider}>
                     <article>
                         {isTranslatedToEnglish? 
                         <>
-                            <p>
-                                I really enjoy programming, reading mangas & comics, 
-                                playing videogames, watching anime & films and listening to music.
-                            </p>
-                            <p>
-                                Here are some of my favourite things.
-                            </p>
+                            <span>Taekwon-Do ITF</span>
+                            <span>Scout (S.Ca.Di)</span>
+                            <span>Mangas</span>
+                            <span>Comics</span>
+                            <span>Videogames</span>
+                            <span>Anime</span>
+                            <span>Films</span>
+                            <span>Music</span>
                         </> : <>
-                            <p>
-                                Disfruto mucho programar, leer mangas y comics, 
-                                jugar videojuegos, ver anime y peliculas, y escuchar música.
-                            </p>
-                            <p>
-                                Aquí dejo algunas de mis cosas favoritas.
-                            </p>
+                            <span>Taekwon-Do ITF</span>
+                            <span>Scout (S.Ca.Di)</span>
+                            <span>Mangas</span>
+                            <span>Comics</span>
+                            <span>Videojuegos</span>
+                            <span>Anime</span>
+                            <span>Películas</span>
+                            <span>Música</span>
                         </>}
                     </article>
                     {hobbies.articles.map((article, articleKey) => (
-                    <article className={s.imgContainer} key={articleKey}>
-                        <img src={hobbies.ARTICLES_IMGS_DIR+article.img} alt="" />
-                        <div className={s.type}>
-                            <span>{article.type}</span>
-                        </div>
-                    </article>))}
+                        <ImgCarousel article={article} key={articleKey} en={isTranslatedToEnglish}/>
+                    ))}
                     <button>
                         {isTranslatedToEnglish? "Show More" : "Ver Más"}
                     </button>
