ef1214789991715676165efaa1641098dfd7c451
McMaldo <maldonado.ignacio.pablo@gmail.com>
2025-03-20 14:41:17 -0300

    FEAT #018 * Link Preview * Link Preview Img * Windsurf icon * SkillSet import fix

--- src/component/skillSet/SkillSet.jsx
+++ src/component/skillSet/SkillSet.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import s from './skillSet.module.css';
 import { useTheme } from "../../hook/useTheme";
 import userData from "../../assets/userData.json";
