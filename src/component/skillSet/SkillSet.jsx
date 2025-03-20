67d2b169a3596ce48cf7a2850fa2452cea94b96a
McMaldo <maldonado.ignacio.pablo@gmail.com>
2025-03-19 21:32:36 -0300

    FEAT #017 * Font Awesome Global Icons * soft skills, programming concepts * general tools => programming tools * skillTagsSimple

--- src/component/skillSet/SkillSet.jsx
+++ src/component/skillSet/SkillSet.jsx
@@ -1,38 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import s from './skillSet.module.css';
 import { useTheme } from "../../hook/useTheme";
 import userData from "../../assets/userData.json";
 import SkillTagMore from "../skillTagDesc/SkillTagMore";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function SkillTag({skill}) {
+	let {theme} = useTheme();
+	const [mostrarComponente, setMostrarComponente] = useState(false);
+	
+	return(
+		skill.more?
+		<div className={s.skillTag+" "+s.skillTagMore}
+			onMouseEnter={() => setMostrarComponente(true)}
+			onMouseLeave={() => setMostrarComponente(false)}
+		>
+			<img src={userData.SKILLTAGS_ICONS_DIR + (theme=="dark"? skill.img : skill.img.replace(/-Dark/g, "-Light"))} alt="" />
+			<div className={s.name}>{skill.name}</div>
+			<div className={s.desc}>{skill.desc}</div>
+			
+			{mostrarComponente? <SkillTagMore skillMore={skill.more} /> : ""}
+		</div>
+		:
+		<div className={s.skillTag}>
+			<img src={userData.SKILLTAGS_ICONS_DIR + (theme=="dark"? skill.img : skill.img.replace(/-Dark/g, "-Light"))} alt="" />
+			<div className={s.name}>{skill.name}</div>
+			<div className={s.desc}>{skill.desc}</div>
+		</div>
+	)
+}
+function SkillTagsCategory({cat, index}) {
+	const [isTranslatedToEnglish] = useLocalStorage("translatedToEnglish", true);
+	const [isExpanded, setExpanded] = useState(index < 3);
+	
+	const toggleExpanded = () => {
+		setExpanded((prevState) => {
+		const newState = !prevState; // Calcula el nuevo estado
+		return newState; // Devuelve el nuevo estado
+		});
+	};
+	
+	return (
+		<details open={isExpanded}>
+		<summary
+			onClick={(e) => {
+				e.preventDefault();
+				toggleExpanded();
+			}}
+		>
+			<FontAwesomeIcon icon="fa-solid fa-caret-right" className={isExpanded ? s.expanded : ""} />
+			<h4>{cat.catName}</h4>
+		</summary>
+		{cat.catName === "Programming Concepts" || cat.catName === "Soft Skills" ? (
+			<div className={s.tagSliderSimple}>
+			{cat.skills.map((skill, skillKey) => (
+				<div className={s.skillTagSimple} key={skillKey}>
+					{isTranslatedToEnglish ? skill.en : skill.es}
+				</div>
+			))}
+			</div>
+		) : (
+			<div className={s.tagSlider}>
+			{cat.skills.map((skill, skillKey) => (
+				<SkillTag skill={skill} key={skillKey} />
+			))}
+			</div>
+		)}
+		</details>
+	);
+}
+	
 
 export default function SkillSet() {
-
-	let {theme} = useTheme();
-
 	return (
 		<div className={s.skillSet}>
 			{userData.skillTags.map((cat, catKey) => (
-				<div key={catKey}>
-					<h4>{cat.catName}</h4>
-					<div className={s.tagSlider}>
-						{cat.skills.map((skill, skillKey) => {
-							const [mostrarComponente, setMostrarComponente] = useState(false);
-							
-							return(
-								<div className={s.skillTag+" "+(skill.more && s.skillTagMore)} key={skillKey}
-									onMouseEnter={() => setMostrarComponente(true)}
-									onMouseLeave={() => setMostrarComponente(false)}
-								>
-									<img src={userData.SKILLTAGS_ICONS_DIR + (theme=="dark"? skill.img : skill.img.replace(/-Dark/g, "-Light"))} alt="" />
-									<div className={s.name}>{skill.name}</div>
-									<div className={s.desc}>{skill.desc}</div>
-									
-									{(mostrarComponente && skill.more) ? <SkillTagMore skillMore={skill.more} /> : ""}
-								</div>
-							)
-						})}
-					</div>
-				</div>
+				<SkillTagsCategory cat={cat} index={catKey}	key={catKey}/>
 			))}
 		</div>
-  )
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import './assets/vars.css';
 import './assets/index.css';
 import Portfolio_v1 from './page/portfolio/Portfolio_v1.jsx';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, faTwitter, faFontAwesome);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Portfolio_v1 />
